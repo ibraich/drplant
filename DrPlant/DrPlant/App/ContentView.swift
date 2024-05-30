6c4c16e4177dc60480794239df61328b51bdf9bd
Adam <adambokun@gmail.com>
2024-05-30 21:51:42 +0200

    initial organisation

---
+++ DrPlant/DrPlant/App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DrPlant
+//
+//  Created by Adam Bokun on 30.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DrPlant/DrPlant/App/DrPlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrPlantApp.swift
+//  DrPlant
+//
+//  Created by Adam Bokun on 30.05.24.
+//
+
+import SwiftUI
+
+@main
+struct DrPlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
